abafafd0f481f5606955242ce4895f0f3e73f1fc
tulg <tulg@mcrew-tech.com>
2022-11-14 20:04:30 +0700

    ss1 done

---
+++ ss1_csdl/create_db_basic.sql
@@ -0,0 +1,15 @@
+--------------------------
+create database studentmanagement;
+use studentmanagement;
+create table class (
+id int primary key, 
+name nvarchar(50)
+);
+create table teacher (
+id int primary key, 
+name nvarchar(50), 
+age int, 
+country nvarchar(50)
+);
+select * from class;
+select * from teacher;
